26f0adc3368a70eff88b1502456d4653b0a31e2c
pratibha1897 <54135702+pratibha1897@users.noreply.github.com>
2022-09-12 01:26:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whosPaying(names) {
   var numberOfPerson = names.length;
   var randomPosition = Math.floor(Math.random() * numberOfPerson);
   var randomPerson = names[randomPosition];
-  console.log(`${randomPerson} is going to buy somethig today!`);
+  document.write(`${randomPerson} is going to buy somethig today!`);
 }
 
 whosPaying();
